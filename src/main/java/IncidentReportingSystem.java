ff528e515312de9c425166fc051aa74888340d0a
Ashish Kumar <ashish@Ashishs-MacBook-Air.local>
2021-03-05 13:35:45 +0530

    main class

---
+++ src/main/java/IncidentReportingSystem.java
@@ -0,0 +1,21 @@
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Slf4j
+@SpringBootApplication(scanBasePackages = {"com.project"})
+@ComponentScan(basePackages = {"com.project"})
+@EntityScan({"com.project"})
+@EnableJpaRepositories({"com.project"})
+@EnableCaching
+
+public class IncidentReportingSystem {
+        public static void main(String[] args) {
+            SpringApplication.run(IncidentReportingSystem.class, args);
+        }
+    }
+
